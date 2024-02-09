0d20e70c4fc8029db15a1228c83421d915c954b6
1-wh <159502059+1-wh@users.noreply.github.com>
2024-02-08 21:41:46 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,111 +1,147 @@
+"use client";
 import Image from "next/image";
+import Panel from "./Panel";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center pt-5 sm:p-5 sm:pt-16 lg:p-32 lg:pt-16">
+      <div className="mb-4 max-w-[600px] lg:max-w-[900px] xl:max-w-[1200px] w-full">
+        <div className="flex items-center overflow-hidden">
+          <Image
+            className="z-20 absolute translate-y-[5.5ch] sm:static sm:translate-y-0 mb-2 ml-5 sm:ml-10 mr-4 rounded-[36px] border-2 border-black/10 dark:border-white/60 p-0.5 bg-gray-100 dark:bg-black sm:bg-transparent sm:dark:bg-transparent"
+            src="/photo.png"
+            alt="NextGPT.live"
+            width={72}
+            height={72}
+            priority
+          />
+          <p className="mx-4 min-w-[388px] text-center sm:text-start font-sans max-w-[56ch] sm:max-w-none">
+            Hello! I'm{" "}
+            <span className="font-semibold font-serif text-2xl">
+              Mike Hyatt
+            </span>
+            . Please feel free to converse with my AI assistant.
+            <span className="hidden sm:inline"> It's here to help!</span>
+          </p>
+        </div>
+        <div className="sm:mx-4 sm:rounded-lg border border-transparent my-4 px-5 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30">
+          <Panel />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="z-10 w-full">
+        <p className="text-center font-semibold text-2xl font-serif">
+          Projects
+        </p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="grid text-left sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href=""
+          className="group rounded-lg border border-transparent max-w-[268px] mx-4 my-4 px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2
+            className={`mb-3 text-2xl font-semibold group-hover:text-blue-500 group-hover:dark:text-blue-400`}
+          >
+            NextGPT<span className="font-mono font-normal">.live</span>{" "}
           </h2>
+          <Image
+            className="mb-2 rounded dark:invert transition-transform group-hover:translate-x-1 motion-reduce:transform-none"
+            src="/nextgpt.png"
+            alt="NextGPT.live"
+            width={150}
+            height={150}
+            priority
+          />
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            An all-encompassing chatbot hosted on AWS.
           </p>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href=""
+          className="group rounded-lg border border-transparent max-w-[268px] mx-4 my-4 px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2
+            className={`mb-3 text-2xl font-semibold group-hover:text-blue-500 group-hover:dark:text-blue-400`}
+          >
+            Doorman{" "}
           </h2>
+          <div className="mb-2 flex w-[150px] h-[150px] justify-center items-center">
+            <Image
+              className="mb-2 rounded transition-transform group-hover:translate-x-1 motion-reduce:transform-none"
+              src="/doorman.png"
+              alt="Doorman"
+              width={50}
+              height={50}
+              priority
+            />
+          </div>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            A voice assistant app for elderly tech coaching.
           </p>
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href=""
+          className="group rounded-lg border border-transparent max-w-[268px] mx-4 my-4 px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2
+            className={`mb-3 text-2xl font-semibold group-hover:text-blue-500 group-hover:dark:text-blue-400`}
+          >
+            UAV Simulation{" "}
           </h2>
+          <Image
+            className="mb-2 rounded transition-transform group-hover:translate-x-1 motion-reduce:transform-none"
+            src="/uav.png"
+            alt="UAV Simulation"
+            width={150}
+            height={150}
+            priority
+          />
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+            A software-in-the-loop simulation for autonomous drone navigation.
           </p>
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href=""
+          className="group rounded-lg border border-transparent max-w-[268px] mx-4 my-4 px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2
+            className={`mb-3 text-2xl font-semibold group-hover:text-blue-500 group-hover:dark:text-blue-400`}
+          >
+            NTsolver{" "}
           </h2>
+          <Image
+            className="mb-2 rounded transition-transform group-hover:translate-x-1 motion-reduce:transform-none"
+            src="/extension.png"
+            alt="NTs extension"
+            width={150}
+            height={150}
+            priority
+          />
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            A learning-based differential equation solver with enhanced
+            accuracy.
           </p>
+        </a>
+      </div>
+      <div className="xl:fixed xl:bottom-0 pt-9 text-sm">
+        <span className="mr-2">LinkedIn</span>
+        <a
+          className="text-blue-500 dark:text-blue-400"
+          href="https://www.linkedin.com/in/whe1/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://www.linkedin.com/in/whe1/
         </a>
       </div>
     </main>
